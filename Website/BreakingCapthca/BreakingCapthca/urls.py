8d6d2144d7d215409aa5d99014d3b51adfc27efb
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2023-04-20 20:35:17 +1000

    Create the home page and other pages such as object detection

--- Website/BreakingCapthca/BreakingCapthca/urls.py
+++ Website/BreakingCapthca/BreakingCapthca/urls.py
@@ -5,6 +5,9 @@ from objectDetection import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('objectDetection/', views.objectDetection, name="objectDetection"),
+    path('textDetection/', views.textDetection, name="textDetection"),
+    path('voiceDetection/', views.voiceDetection, name="voiceDetection"),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls'))
 ]
--- Website/BreakingCapthca/objectDetection/views.py
+++ Website/BreakingCapthca/objectDetection/views.py
@@ -4,3 +4,15 @@ from django.http import Http404
 
 def home(request):
     return render(request, 'home.html')
+
+
+def objectDetection(request):
+    return render(request, 'objectDetection.html')
+
+
+def textDetection(request):
+    return render(request, 'textDetection.html')
+
+
+def voiceDetection(request):
+    return render(request, 'voiceDetection.html')
