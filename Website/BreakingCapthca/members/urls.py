8a0b5721a84080ea8b9d24fc730b262ed8fe656b
nik2507 <nik250795@gmail.com>
2023-04-24 01:31:45 +1000

    code changes

--- Website/BreakingCapthca/members/urls.py
+++ Website/BreakingCapthca/members/urls.py
@@ -8,8 +8,16 @@ urlpatterns = [
     path('logout_user', views.logout_user, name='logout'),
     path('register_user', views.register_user, name='register'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='members/reset_password.html'), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+    path('reset_password/', 
+        auth_views.PasswordResetView.as_view(template_name="members/password_reset.html"),
+        name="reset_password"),
+    path('reset_password_sent/', 
+         auth_views.PasswordResetDoneView.as_view(template_name="members/password_reset_sent.html"), 
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>', 
+         auth_views.PasswordResetConfirmView.as_view(template_name="members/password_reset.html"), 
+         name="password_reset_confirm"),
+    path('reset_password_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name="members/password_reset.html"), 
+         name="password_reset_complete")
 ]
