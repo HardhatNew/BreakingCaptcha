303b6c9318aeed629a7d62f4b188a8993c620a8d
nik2507 <nik250795@gmail.com>
2023-05-04 19:42:29 +1000

    password change updates

--- Website/BreakingCapthca/members/urls.py
+++ Website/BreakingCapthca/members/urls.py
@@ -8,16 +8,18 @@ urlpatterns = [
     path('logout_user', views.logout_user, name='logout'),
     path('register_user', views.register_user, name='register'),
 
-    path('reset_password/', 
-        auth_views.PasswordResetView.as_view(template_name="members/password_reset.html"),
+    path('reset_password/',  # form to submit email
+        auth_views.PasswordResetView.as_view(template_name="authenticate/password_reset.html"),
         name="reset_password"),
-    path('reset_password_sent/', 
+    path('reset_password_sent/', # form which says email sent
          auth_views.PasswordResetDoneView.as_view(template_name="authenticate/password_reset_sent.html"), 
          name="password_reset_done"),
-    path('reset/<uidb64>/<token>', 
-         auth_views.PasswordResetConfirmView.as_view(template_name="members/password_reset_C.html"), 
+
+     # uidb64 = user id encoded in base64
+    path('reset/<uidb64>/<token>',  # form which allows for password to be reset
+         auth_views.PasswordResetConfirmView.as_view(template_name="authenticate/password_reset_C.html"), 
          name="password_reset_confirm"),
-    path('reset_password_complete/', 
-         auth_views.PasswordResetCompleteView.as_view(template_name="members/password_reset.html"), 
+    path('reset_password_complete/',  #form which confirms password has been reset
+         auth_views.PasswordResetCompleteView.as_view(template_name="authenticate/password_reset.html"), 
          name="password_reset_complete")
 ]
